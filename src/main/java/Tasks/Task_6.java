7e6956b05061780908bc23b840cf96df6bfbd7d7
IKozlova <bezzubtseva.ira@yandex.ru>
2023-12-20 15:37:30 +0300

    Решение 6-ой задачи

---
+++ src/main/java/Tasks/Task_6.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+/**
+ * Вывести на экран результат вычисления: 1 + 2 - 4
+ */
+
+public class Task_6 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 4;
+        int result = a + b - c;
+        System.out.println(result);
+    }
+}
